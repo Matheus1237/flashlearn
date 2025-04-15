033f51fffb6e8736c704ab660191b2046a2bd757
Matheus1237 <masian202207@gmail.com>
2025-04-14 21:10:05 -0300

    Configura banco de dados e CSRF para produção

--- setup/settings.py
+++ setup/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -149,3 +149,4 @@ if not DEBUG:
     SECURE_BROWSER_XSS_FILTER = True
     SECURE_CONTENT_TYPE_NOSNIFF = True
     X_FRAME_OPTIONS = 'DENY'
+    CSRF_TRUSTED_ORIGINS = ['https://flashlearn-x0jz.onrender.com']
